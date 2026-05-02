c0af5ff232c892962922ab5ed8e3a44246f80a7c
ananthchellappa <ananth.chellappa@outlook.com>
2026-05-01 20:14:55 -0700

    handle both facings

--- arrow.tcl
+++ arrow.tcl
@@ -1,7 +1,3 @@
-# Not as nice as what I have in Cadence, but a good start
-# Select the pin (must have a whisker - path - touching it) on symbol, and an arrow will be added to the whisker, pointing left
-# So, you'd want to use this for an output port on left edge or input port on right edge of symbol
-
 proc sel_rect { x y object_type } {
 	# path, port, wire, etc..
 	allowselect none
@@ -28,9 +24,11 @@ proc arrow {} {
 	find port -scope selection -count -goto none
 	set x_p [property get -system X]
 	set y_p [property get -system Y]
+	set orient [property get TextJustification.Horizontal -system]
 	sel_rect $x_p $y_p path
 
 	set x [ expr min( [lindex [split [join [property get -system Vertices] ] , ] 0] , [lindex [split [join [property get -system Vertices] ] , ] 2 ] ) ]
+	set x_max [ expr max( [lindex [split [join [property get -system Vertices] ] , ] 0] , [lindex [split [join [property get -system Vertices] ] , ] 2 ] ) ]
 	set y [lindex [split [join [property get -system Vertices] ] , ] 1]	 ;# only supports horizontal lines :-)
 	delete
 	set saved_sn_gr [expr { ( [setup schematicgrid get -snapgridsize] ) * 1.0 * [setup schematicunits get -numerator]/ [setup schematicunits get -denominator] } ]
@@ -38,8 +36,13 @@ proc arrow {} {
 	mode draw path
 	
 	mode -drawstyle aa; # aa = any angle
-	point click $x_p $y_p -units iu
-	point click2 [expr $x+2] $y -units iu
+	if {"Right" eq $orient} {
+		point click [expr $x+2] $y -units iu
+		point click2 $x_max $y -units iu
+	} else {
+		point click $x_p $y_p -units iu
+		point click2 [expr $x+2] $y -units iu
+	}
 	point click $x $y -units iu
 	point click [expr $x+4] [expr $y-2] -units iu
 	point click [expr $x+2] $y -units iu
