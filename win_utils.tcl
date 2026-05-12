bfe977c4f4388ad2cfe51f2534e3d04d163528bc
ananthchellappa <ananth.chellappa@outlook.com>
2026-05-11 23:35:06 -0700

    comment on return list structure
    Clarify the comment to specify the format of the return value.

--- win_utils.tcl
+++ win_utils.tcl
@@ -1,5 +1,5 @@
 proc win_viewportrect_iu {} {
-# return view port coordinates in integer units
+# return view port coordinates in integer units. Flat list x1 y1 x2 y2
 	set save_dispunits [setup schematicunits get -displayunits]
 	mode renderoff
 	setup schematicunits set -displayunits iu
